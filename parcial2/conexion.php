bc3c76073abca36a97a7b75756980d93698c3a59
ApodacaHasly <124411148+ApodacaHasly@users.noreply.github.com>
2023-10-18 19:10:36 -0700

    18/10/23

--- parcial2/conexion.php
+++ parcial2/conexion.php
@@ -3,11 +3,12 @@
    $server= "localhost";
    $user = "root";
    $password = "";
+   $baseDatos = "cetis107";
 
-   $conexion = new mysqli($server, $user, $password);
+   $conexion = new mysqli($server, $user, $password, $baseDatos);
 
    if($conexion->connect_error){
-    die("fallo la conexion" . $conexion);
+    die("fallo la conexion" . $conexion->connect_error);
    }
 
 
